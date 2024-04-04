9711bac11a859f99f1f8452ea9ce4a99ba353cc6
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-04 16:24:10 +0530

    .

---
+++ Day3/index.js
@@ -0,0 +1,41 @@
+// console.log("hello");
+//  var Uname = prompt("kulkaml enter your name");
+//  console.log(Uname);
+// function env(){
+//     // for(var i=0;i<10;i++){
+//         for(let i=0;i<10;i++){
+//         console.log(i*2);
+//     }
+    
+// }
+
+
+// function print(str){
+//     console.log(str);
+// }
+// const s='hello ravi';
+// print(s);
+
+
+// function declaration /
+// function print(){
+//     console.log('--');
+// }
+
+// // function assignment (named)
+// const a=function print(){
+//     console.log('--');
+// }
+
+// // function assignment (anonymous)
+// const b=function (){
+//     console.log('--');
+// }
+
+
+// // arrow funciton assgnment
+// let c = () =>{
+//     console.log('--');
+// }
+// c();
+
