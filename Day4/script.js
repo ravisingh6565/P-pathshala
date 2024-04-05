e4b8ae690503345bda450a88dda44208262e0dde
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-05 10:30:46 +0530

    .

--- Day4/script.js
+++ Day4/script.js
@@ -23,3 +23,35 @@
 // document.write(str);
 
 
+// const obj = {
+//     name:'ravi',
+//     lastName: 'singh'
+// }
+
+// obj.name = 2020;
+// console.log(obj);
+
+//  let arr = [2,3,4]; 
+//  arr[20] = 'ravi';
+//  console.log(arr);
+
+
+const obj = {
+    name:'ravi',
+    lastName: 'singh'
+}
+
+
+ let x = [2,3,4]; 
+//  arr[2] = 111;
+//  console.log(typeof(obj))
+//  console.log(typeof(arr));
+
+function objOrNot(x){
+    if(Array.isArray(x) === false){
+        console.log("it is object");
+    }else{
+        console.log("it is array");
+    }
+}
+objOrNot(x);
