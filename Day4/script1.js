7b5678319b1848086a389e208c34d49d62cdae09
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-05 14:41:19 +0530

    .

---
+++ Day4/script1.js
@@ -0,0 +1,23 @@
+// // const res = document.querySelector('div');
+// // // console.log(div);
+// // // div.remove();
+// // const c = document.querySelector('p');
+// // res.removeChild(c);
+
+
+function getInfo(e){
+    // console.log("butotn clicked");
+    // const ne = document.createElement("h2");
+    // ne.innerText = 'new h2 is added';
+    // document.body.prepend(ne);
+// const firstDiv  = document.getElementsByTagName("div");
+// firstDiv[0].appendChild(ne);
+e.target.style.backgroundColor = 'green';
+e.target.style.color = 'red';
+e.target.style.marginLeft ='500px';
+}
+
+
+
+
+
