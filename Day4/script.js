4fdb009b38e93c642d223ed34904b65242240b1f
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-05 10:03:26 +0530

    .

--- Day4/script.js
+++ Day4/script.js
@@ -1,9 +1,25 @@
-let a = new String("hello");
-let b = "hello";
+// let a = new String("hello");
+// let b = "hello";
 
-if(a===b){
-    console.log("true");
+// if(a===b){
+//     console.log("true");
 
-}else{
-    console.log("no");
-}
+// }else{
+//     console.log("no");
+// }
+
+// const obj = {
+//     name:'ravi',
+//     lastName: 'singh'
+// }
+
+// let obj = {
+//    " name":"ravi",
+//     "lastName": "singh"
+// }
+
+// console.log(obj);
+// const str = JSON.stringify(obj);
+// document.write(str);
+
+
