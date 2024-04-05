405e96ced8871e5b2ad95e436ea907c3a930c002
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-05 11:02:00 +0530

    .

--- Day4/script.js
+++ Day4/script.js
@@ -36,26 +36,74 @@
 //  console.log(arr);
 
 
-const obj = {
-    name: 'ravi',
-    lastName: 'singh'
-}
 
-
-let x = [2, 3, 4];
 //  arr[2] = 111;
 //  console.log(typeof(obj))
 //  console.log(typeof(arr));
 
-function objOrNot(x) {
-    if (Array.isArray(x) !== false) {
-        console.log("it is array");
-    } else if (typeof (x) != "object") {
-        console.log("it is " + typeof (x));
-    }
-    else {
-        console.log("it is object");
-    }
+// function objOrNot(x) {
+//     if (Array.isArray(x) !== false) {
+//         console.log("it is array");
+//     } else if (typeof (x) != "object") {
+//         console.log("it is " + typeof (x));
+//     }
+//     else {
+//         console.log("it is object");
+//     }
 
+// }
+// objOrNot(5);
+
+
+// for( let i =0;i<2;i++){
+//     console.log(x[i]);
+// }
+
+// for( let i =0;i<2;i++){
+//     console.log(obj[i]);
+// }
+
+// for( let i of x){
+//     console.log(i);
+// }
+
+
+// for( let i of obj){
+//     console.log(obj);
+// }
+
+
+
+// for( let i in x){
+//     console.log(i);
+// }
+
+// for( let i in obj){
+//     console.log(obj);
+// }
+
+
+
+
+const obj = {
+    name: 'ravi',
+    lastname: 'singh'
 }
-objOrNot(5);
+  
+let arr = [2, 3, 4];
+
+
+
+
+
+
+
+// *****************Document Object model *********************************//
+
+
+
+
+
+
+
+
