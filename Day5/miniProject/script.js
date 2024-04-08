7affa42c884e10b7122438657c96ad677ff9e411
ravisingh6565 <ravisuneel6262@gmail.com>
2024-04-08 14:47:07 +0530

    .

--- Day5/miniProject/script.js
+++ Day5/miniProject/script.js
@@ -1,26 +1,33 @@
 console.log("...API STARTED");
 
-function callAPI(){
-  fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=ffc3755c37c147799c44793024549eec")
-  .then((res)=>{
-     return res.json();
-  })
-  .then((data)=>{
-    // console.log(data);
-    renderUI(data);
-  })
+function callAPI() {
+    fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=ffc3755c37c147799c44793024549eec")
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            renderUI(data);
+        })
 }
 
 // callAPI();
 
-function renderUI(data){
+function renderUI(data) {
+
     const root = document.getElementById("root");
 
-    const div = document.createElement("div");
-    div.innerText = 'card';
+    const arr = data.articles[0];
+    // console.log(arr[0]);
 
+    const div = document.createElement("div");
+    const h3   = document.createElement("h3");
+    h3.innerText = arr.title;
+    div.appendChild(h3);
+
+    // div.innerText = arr.title;
     root.appendChild(div);
-    console.log(data);
+
 }
 
 // renderUI();
